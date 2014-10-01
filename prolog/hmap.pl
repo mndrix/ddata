9e913c7d14a139e1b2e90da7d3f735aa68e2fc6c
Michael Hendricks <michael@ndrix.org>
2014-10-01 12:49:56 -0600

    Simplify kv implementation
    Now that the base case is defined in terms of Mask=1 and we use all bits
    in the original hash, we can remove the partial hash argument.

--- prolog/hmap.pl
+++ prolog/hmap.pl
@@ -6,16 +6,15 @@ kv(Map,Key,Value) :-
     term_hash(Key,Hash),
     Depth is floor(log(Hash)/log(2)),  % log2(Hash)
     Mask is 1 << Depth,
-    Partial is Hash,
-    kv(Mask,Partial,Map,Hash,Key,Value).
+    kv(Mask,Map,Hash,Key,Value).
 
-kv(1,_P,node(Hash,Key,Value,_L,_R),Hash,Key,Value) :-
+kv(1,node(Hash,Key,Value,_L,_R),Hash,Key,Value) :-
     !.
-kv(Mask0,P,node(_H,_K,_V,L,R),Hash,Key,Value) :-
+kv(Mask0,node(_H,_K,_V,L,R),Hash,Key,Value) :-
     Mask0 >= 1,
-    Mask1 is Mask0 >> 1,
     ( 0 is Hash /\ Mask0 -> Child=L; Child=R ),
-    kv(Mask1,P,Child,Hash,Key,Value).
+    Mask1 is Mask0 >> 1,
+    kv(Mask1,Child,Hash,Key,Value).
 
 
 show(Map) :-
