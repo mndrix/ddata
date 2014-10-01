99576bdd9212ac642e28557db75b4680b67407c3
Michael Hendricks <michael@ndrix.org>
2014-10-01 12:43:09 -0600

    Fix quickcheck test
    Duplicate keys are not allowed.

--- t/quickcheck.pl
+++ t/quickcheck.pl
@@ -1,13 +1,18 @@
 :- use_module(library(hmap)).
+:- use_module(library(random), [random_permutation/2]).
 
-prop_insert_exists(Keys:list(integer)) :-
-    % TODO remove duplicate keys
+prop_insert_exists(Ks:list(integer)) :-
+    dedup(Ks,Keys),
     same_length(Keys,Vals),
     maplist(arbitrary(integer),Vals),
     
     maplist(kv(Map),Keys,Vals),
     maplist(kv(Map),Keys,Got),
     Vals == Got.
+
+dedup(Xs,Unique) :-
+    sort(Xs,Sorted),
+    random_permutation(Sorted,Unique).
 
 :- use_module(library(quickcheck)).
 :- use_module(library(tap)).
