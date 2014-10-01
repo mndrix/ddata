fb76eceaaf00cce224ee43bc40b5542572167bb5
Michael Hendricks <michael@ndrix.org>
2014-10-01 13:49:11 -0600

    Use 8-ary trees
    This is 31% faster than 4-ary trees.
    
    I tried upgrading to 16-ary trees but that gave slower performance than
    8-ary trees.  I'll stay with 8-ary for a while.

--- prolog/hmap.pl
+++ prolog/hmap.pl
@@ -4,7 +4,7 @@
 
 kv(Map,Key,Value) :-
     term_hash(Key,Hash),
-    Depth is floor(log(Hash)/log(4)),  % log4(Hash)
+    Depth is floor(log(Hash)/log(8)),  % log8(Hash)
     kv(Depth,Hash,Map,Hash,Key,Value).
 
 kv(0,_P,Node,Hash,Key,Value) :-
@@ -13,15 +13,15 @@ kv(0,_P,Node,Hash,Key,Value) :-
 kv(Depth,P,Node,Hash,Key,Value) :-
     Depth > 0,
     node(Node,_,_,_),
-    N is 4 + (P /\ 0b11),
+    N is 4 + (P /\ 0b111),
     arg(N,Node,Child),
     Depth1 is Depth - 1,
-    P1 is P >> 2,
+    P1 is P >> 3,
     kv(Depth1,P1,Child,Hash,Key,Value).
 
 
 node(Node,Hash,Key,Value) :-
-    functor(Node,node,7 /*3+4*/),
+    functor(Node,node,11 /*3+8*/),
     arg(1,Node,Hash),
     arg(2,Node,Key),
     arg(3,Node,Value).
@@ -35,14 +35,16 @@ show(Map,Indent) :-
     !,
     indent(Indent),
     format(".~n").
-show(node(_H,K,V,A,B,C,D),Indent) :-
+show(Node,Indent) :-
+    node(Node,_,K,V),
     indent(Indent),
     format("~p => ~p~n", [K,V]),
     succ(Indent,NextIndent),
-    show(A,NextIndent),
-    show(B,NextIndent),
-    show(C,NextIndent),
-    show(D,NextIndent).
+    forall( between(4,11,N)
+          , ( arg(N,Node,X)
+            , show(X,NextIndent)
+            )
+          ).
 
 indent(N) :-
     forall( between(1,N,_), write("    ") ).
