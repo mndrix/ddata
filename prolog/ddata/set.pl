5c8f6060bd270681ae37017179b129b6f4ae62c6
Michael Hendricks <michael@ndrix.org>
2014-10-07 08:35:38 -0600

    notes for set implementation

---
+++ prolog/ddata/set.pl
@@ -0,0 +1,6 @@
+:- module(ddata_set, [member/2]).
+:- use_module(library(ddata/map), []).
+
+% TODO implement member/2
+% TODO support elements defined by a predicate? `integer(E), between(E,1,10)`
+% TODO implement contains(Superset, Subset)
