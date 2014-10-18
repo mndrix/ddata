5a6a21e80e5999b99e9c91595c61cde1569395c1
Michael Hendricks <michael@ndrix.org>
2014-10-18 07:32:22 -0600

    Getting closer to a working pmap
    This implementation still doesn't pass any tests.  It is cleaner and
    more declarative and should be able to run in multiple directions (with
    slight massaging).

--- prolog/ddata/pmap.pl
+++ prolog/ddata/pmap.pl
@@ -7,6 +7,7 @@ delta(Key,Value,Without,With) :-
     insert(0,Hash,Key,Value,Without,With).
 
 
+trim(trim(_,_,_,_)).
 trim_depth(trim(Depth,_,_,_), Depth).
 trim_hash(trim(_,Hash,_,_), Hash).
 trim_key(trim(_,_,Key,_), Key).
@@ -15,6 +16,24 @@ trim_value(trim(_,_,_,Value), Value).
 
 parent(P) :-
     functor(P,parent,8).
+
+
+empty_parent(P) :-
+    parent(P),
+    foreach(between(1,8,N),arg(N,P,empty)).
+
+
+differ_in_one_child(A0,B0,N,ChildA,ChildB) :-
+    parent(A0),
+    parent(B0),
+    nth_child(N,A0,ChildA),
+    nth_child(N,B0,ChildB),
+    map_args(differ_(N),A0,B0).
+
+differ_(N,N,A,B) :-
+    dif(A,B).
+differ_(N,M,A,A) :-
+    dif(N,M).
 
 
 map_children(P,Goal) :-
@@ -65,64 +84,36 @@ map_args_(N,Goal,TermA,TermB) :-
     map_args_(N0,Goal,TermA,TermB).
 
 
-unify_except_arg(ExceptN,X,Y) :-
-    map_args(unify_except_arg_(ExceptN),X,Y).
-
-unify_except_arg_(N,N,_,_).
-unify_except_arg_(ExceptN,N,X,X) :-
-    N \= ExceptN.
-
-
-/*
-%% unknown_key(-K,?V,?Without,?With)
-unknown_key(K,V,empty,Trim) :-
-    % With has 1 key
-    trim_depth(Trim,0),
-    trim_key(Trim,K),
-    trim_value(Trim,V).
-unknown_key(K,V,Peer0,Parent) :-
-    % With has 2 keys
-    trim_depth(Peer0,0),
-    trim_key(Peer0,Kpeer),
-    dif(K,Kpeer),
-    ( % peers belong in different subtrees
-      trim_pushdown(1,Peer0,Parent),
-      trim_depth(Us,0),
-      trim_key(Us,Key),
-      trim_value(Us,Value),
-      trim_pushdown(Us,Parent),
-      todo("other children are empty/0")
-
-    ; % peers belong in same subtree
-      single_child(Parent,Child),
-      unknown_key(Kpeer,Vpeer,Peer0,Child),
-      unknown_key(K,V,Trim0,Child)
-    ).
-unknown_key(K,V,ParentA,ParentB) :-
-    % With has >2 keys
-    todo.
-*/
-
 
 %% known_key(+Depth:nonneg,+Hash,+Key,?Value,?Without,?With)
 insert(Depth,Hash,Key,Value,empty,Trim) :-
     Trim=trim(Depth,Hash,Key,Value).
-insert(Depth,Hash,K,V,Trim,Parent) :-
+insert(Depth,Hash,K,V,Trim,With) :-
+    parent(With),
     trim_depth(Trim,Depth),
-    parent(Parent),
-    trim_pushdown(Trim,Parent0),
-    insert(Depth,Hash,K,V,Parent0,Parent).
-insert(Depth0,Hash,K,V,Parent0,Parent) :-
-    succ(Depth0,Depth),
-    hash_depth_n(Hash,Depth,N),
-    nth_child(Parent0,N,Child0),
-    nth_child(Parent,N,Child),
-    unify_except_arg(N,Parent0,Parent),
-    insert(Depth,Hash,K,V,Child0,Child).
+    trim_hash(Trim,TrimHash),
+    ( ground(TrimHash) ->
+        trim_pushdown(Trim,Without),
+        insert(Depth,Hash,K,V,Without,With)
+    ; otherwise ->
+        parent(Without),
+        insert(Depth,Hash,K,V,Without,With),
+        trim_pushdown(Trim,Without)
+    ).
+insert(Depth0,Hash,K,V,Without,With) :-
+    parent(Without),
+    parent(With),
+    hash_depth_n(Depth0,Hash,N),
+    differ_in_one_child(Without,With,N,Old,New),
+    succ(Depth0,Depth1),
+    insert(Depth1,Hash,K,V,Old,New).
 
 
 hash_depth_n(Hash,Depth,N) :-
-    N is (Hash >> (3*Depth)) /\ 0b111.
+    when((ground(Hash),ground(Depth)), hash_depth_n_(Hash,Depth,N)).
+
+hash_depth_n_(Hash,Depth,N) :-
+    N is ((Hash >> (3*Depth)) /\ 0b111) + 1.
 
 
 kv(Map,Key,Value) :-
@@ -135,45 +126,22 @@ kv(_Map,_Key,_Value) :-
 
 % trim_pushdown(?Trim,?AsParent)
 trim_pushdown(Trim0,AsParent) :-
-    Trim0 = trim(Depth,Hash,Key,Value),
     parent(AsParent),
-    succ(Depth,Depth1),
-    Trim = trim(Depth1,Hash,Key,Value),
-    hash_depth_n(Hash,Depth,N),
-    arg(N,AsParent,Trim),
-    map_args(trim_pushdown_(N),AsParent).
 
-trim_pushdown_(N,N,_).
-trim_pushdown_(N,M,empty) :-
-    N \= M.
+    % describe trim element at Depth0
+    trim_depth(Trim0,Depth0),
+    trim_hash(Trim0,Hash),
+    trim_key(Trim0,Key),
+    trim_value(Trim0,Value),
 
+    % describe trim element at Depth + 1
+    succ(Depth0,Depth1),
+    trim_depth(Trim1,Depth1),
+    trim_hash(Trim1,Hash),
+    trim_key(Trim1,Key),
+    trim_value(Trim1,Value),
 
-/*
-kv(Map,Key,Value) :-
-    var(Key),
-    !,
-    unknown_key(Map,Key,Value).
-kv(Map,Key,Value) :-
-    hash(Key,Hash),
-    % n = Hash + 1, because hashes can be zero
-    hash_depth(Hash+1,Depth),
-    kv(Depth,Hash,Map,Key,Value).
-
-kv(0,_P,Node,Key,Value) :-
-    node(Node,ExistingKey,ExistingValue),
-    !,
-    ( Key = ExistingKey -> true; throw(collision(Key,ExistingKey)) ),
-    Value = ExistingValue.
-kv(Depth,P,Node,Key,Value) :-
-    % postpone walking the tree until later
-    attr(Node,lazy_kv(Depth,P,Key,Value)),
-    !.
-kv(Depth,P,Node,Key,Value) :-
-    Depth > 0,
-    node(Node,_,_),
-    N is 3 + (P /\ 0b111),
-    arg(N,Node,Child),
-    Depth1 is Depth - 1,
-    P1 is P >> 3,
-    kv(Depth1,P1,Child,Key,Value).
-*/
+    % relate empty parent to parent containing deeper trim element
+    hash_depth_n(Hash,Depth0,N),
+    empty_parent(Empty),
+    differ_in_one_child(Empty,AsParent,N,empty,Trim1).
