04a0a6d519dd0be6375ef88d8c1139594f429f12
Michael Hendricks <michael@ndrix.org>
2016-10-04 12:50:27 -0400

    map: make "huge" test larger
    Now that we only use 64 bits for hashing, I want to confident that we
    can generate even larger maps without routine collisions.

--- t/map/huge.pl
+++ t/map/huge.pl
@@ -4,7 +4,7 @@
 
 % generate a big pile of keys for inserting into a map
 lots_of_keys(Keys) :-
-    length(L,10_000),
+    length(L,100_000),
     maplist(random,L),
     sort(L,Keys).  % remove duplicates
 
